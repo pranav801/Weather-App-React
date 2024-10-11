11175dd4436bc1978941daaac92fd19d3388d93e
pranav801 <pranavtv801@gmail.com>
2024-10-11 17:11:24 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-4xl'> hello</h1>
+    </>
+   
+   
+  )
+}
+
+export default App
